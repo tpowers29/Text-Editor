be5210b1788834185c3f2c3f05f42af7bec988ec
tpowers29 <113735451+tpowers29@users.noreply.github.com>
2023-02-23 13:16:23 -0500

    added install.js and index.js files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
+const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
+if (!promptEvent) {
+ return;
+}
+
 window.addEventListener('appinstalled', (event) => {});
+window.deferredPrompt = null;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,7 +66,7 @@ module.exports = () => {
             plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
           },
         
-      ],
+        ],
     },
   };
 };
